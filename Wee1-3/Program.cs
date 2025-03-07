aa9370994955339d3025b8da73d1c77941a4d575
asecer79 <asecer79@DESKTOP-TPOST9I>
2025-03-07 15:46:57 +0300

    u

--- Wee1-3/Program.cs
+++ Wee1-3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Wee1_3
+{
+    class Program
+    {
+        //entry point// giriş noktası fon.
+        static void Main(string[] args)
+        {
+
+            
+
+
+            Console.WriteLine("Hello world");
+        }
+    }
+}
